94e6de400d33e62433376d84724241fa2a453e11
Shashank <shashank.shekhar.barik@suiit.ac.in>
2020-03-26 12:59:16 +0530

    ver 1.0

---
+++ src/components/SpeechInput.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-layout row wrap justify-space-around>
+        <v-flex xs8 sm9 text-xs-center>
+          <!-- error notification -->
+          <p v-if="error" class="red--text">{{error}}</p>
+          <!-- Run time Text in speech box -->
+          <p v-else class="mb-0">
+            <span>{{runtimeTranscription}}</span>
+          </p>
+        </v-flex>
+        <!-- On Off Toggle Btn -->
+        <v-flex xs2 sm1 text-xs-center>
+          <v-btn
+            dark
+            @click.stop="toggle ? endSpeechRecognition() : startSpeechRecognition()"
+            icon
+            :color="!toggle ? 'grey' : (speaking ? 'red' : 'red darken-3')"
+            :class="{'animated infinite pulse': toggle}"
+          >
+            <!-- <v-icon>{{ toggle ? 'mdi-download' : 'Start Recording' }}</v-icon> -->
+            <v-icon v-if="toggle">mdi-microphone-off</v-icon>
+            <v-icon v-else>mdi-microphone</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+let recognition = SpeechRecognition? new SpeechRecognition() : false
+
+export default {
+  name: 'SpeechInput',
+  props: {
+    lang: {
+      type: String,
+      default: 'en-US'
+    },
+    text: {
+      type: [String, null],
+      required: false
+    }
+  },
+  data () {
+    return {
+      error: false,
+      speaking: false,
+      toggle: false,
+      runtimeTranscription: '',
+      sentences: [],
+    }
+  },
+  methods: {
+      
+    checkCompatibility () {
+      if (!recognition) {
+        this.error = 'Speech Recognition is not available on this browser. Please use Chrome or Firefox'
+        console.log(this.error);
+      }
+    },
+    endSpeechRecognition () {
+      recognition.stop()
+      this.toggle = false
+      this.$emit('speechend', {
+        sentences: this.sentences,
+        text: this.sentences.join('. ')
+      })
+    },
+    startSpeechRecognition () {
+      if (!recognition) {
+        this.error = 'Speech Recognition is not available on this browser. Please use Chrome or Firefox'
+        return false
+      }
+      this.toggle = true
+      recognition.lang = this.lang
+      recognition.interimResults = true
+
+      recognition.addEventListener('speechstart', event => {
+        this.speaking = true
+      })
+
+      recognition.addEventListener('speechend', event => {
+        this.speaking = false
+      })
+
+      recognition.addEventListener('result', event => {
+        const text = Array.from(event.results).map(result => result[0]).map(result => result.transcript).join('')
+        this.runtimeTranscription = text
+      })
+
+      recognition.addEventListener('end', () => {
+        if (this.runtimeTranscription !== '') {
+          this.sentences.push(this.capitalizeFirstLetter(this.runtimeTranscription))
+          this.$emit('update:text', `${this.text}${this.sentences.slice(-1)[0]}. `)
+        }
+        this.runtimeTranscription = ''
+        recognition.stop()
+        if (this.toggle) {
+          // keep it going.
+          recognition.start()
+        }
+      })
+      recognition.start()
+    },
+    capitalizeFirstLetter (string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+  },
+  mounted () {
+    this.checkCompatibility()
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app id="app">
+    <v-container fluid>
+      <v-layout row wrap justify-center class="mt-4">
+        <h1 class="display-1">Vue Speech Input</h1>
+        <img src="@/assets/logo.png">
+        <!-- Input Text Field -->
+        <v-flex xs12 sm10 text-xs-center>
+          <v-text-field
+            label="Speech to Text"
+            v-model="text"
+            textarea
+            outlined
+          ></v-text-field>
+        </v-flex>
+        <!-- Speech Input Component -->
+        <v-flex xs12 sm8 md4 text-xs-center>
+          <SpeechInput :text.sync="text" en-US @speechend="speechEnd" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import SpeechInput from "@/components/SpeechInput"
+
+export default {
+  name: 'Home',
+  components: {
+    SpeechInput
+  },
+  data () {
+    return {
+      text: '',
+      sentences: null
+    }
+  },
+  methods: {
+    speechEnd ({sentences, text}) {
+      console.log('text', text)
+      console.log('sentences', sentences)
+      this.sentences = sentences
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
