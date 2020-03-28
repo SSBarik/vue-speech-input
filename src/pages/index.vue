66a1971bf4539a0292a1a5083b290114ace3a32d
Shashank <shashank.shekhar.barik@suiit.ac.in>
2020-03-28 13:32:10 +0530

    making heroku ready

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,28 @@
 <template>
   <v-app id="app">
     <v-container fluid>
-      <v-layout row wrap justify-center class="mt-4">
-        <h1 class="display-1">Vue Speech Input</h1>
-        <img src="@/assets/logo.png">
+      <div class="text-center">
+      <h1 class="display-1">Vue Speech Input</h1>
+      <p class="pa-2">Press the mic to start recording...</p>
+      <img :src="require('@/assets/logo.png')" height="100">
+      </div>
+      
+      <v-layout row wrap justify-center class="mt-4 pa-2">
         <!-- Input Text Field -->
         <v-flex xs12 sm10 text-xs-center>
-          <v-text-field
+          <v-textarea
             label="Speech to Text"
             v-model="text"
             textarea
             outlined
-          ></v-text-field>
+          ></v-textarea>
         </v-flex>
         <!-- Speech Input Component -->
         <v-flex xs12 sm8 md4 text-xs-center>
           <SpeechInput :text.sync="text" en-US @speechend="speechEnd" />
         </v-flex>
       </v-layout>
+
     </v-container>
   </v-app>
 </template>
@@ -47,5 +52,7 @@ export default {
 </script>
 
 <style>
-
+#app {
+  background-color: #FAFAFA;
+}
 </style>
